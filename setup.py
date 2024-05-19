3845539194fa689bc2caa9e0483766ea8804ef70
jkaye99 <126279637+jkaye99@users.noreply.github.com>
2024-05-19 12:30:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     keywords=["apscheduler", "scheduler", "scheduling", "cron"],
     python_requires=">=3.8",
     install_requires=["flask>=2.2.5,<4.0.0",
-                      "apscheduler>=4.0.0", "python-dateutil>=2.4.2"],
+                      "apscheduler==4.0.0a5", "python-dateutil>=2.4.2"],
     package_data={"Flask-APScheduler": ["LICENSE"]},
     include_package_data=True,
     classifiers=[
